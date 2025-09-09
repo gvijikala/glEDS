c741f58e75a8c9697ba469ee940d676fb59548f7
G Vijikala <g.vijikala@globallogic.com>
2025-09-09 16:09:03 +0530

    editor-support.js file added

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -1,9 +1,8 @@
 // add delayed functionality here
 export default function init({ document }) {
-    console.log('Universal Editor support script loaded');
   document.querySelectorAll('h1').forEach((el) => {
-    el.setAttribute('data-editor-item-id', 'hero.title');
-    el.setAttribute('data-editor-type', 'text');
-    el.setAttribute('data-editor-label', 'Hero Title');
-  });
+  el.setAttribute('data-editor-item-id', 'hero.title');
+  el.setAttribute('data-editor-type', 'text');
+  el.setAttribute('data-editor-label', 'Hero Title');
+});
 }
